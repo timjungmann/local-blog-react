472b39ea5c4fc722bf985d2dc507d2a7eb803482
Tim Jungmann <tim-j11@web.de>
2021-08-23 16:36:53 +0200

    fixed mobile bugs - modal & help

--- src/Components/Help.jsx
+++ src/Components/Help.jsx
@@ -7,7 +7,7 @@ export default function Help({help}) {
         <p>MoodPad lets you put down your thoughts and feelings in a secure and easy way. All entries are only stored locally on your machine. Clearing your cache will delete them, so be careful.</p>
         <p>Use the sliders (R, G, B) to select a color to accompany your writing or just leave it plain white. </p>
         <p>Click the title of any entry to enlarge it.</p>
-        <p>Edit, delete or download an entry with the according buttons.</p>
+        <p>Edit or delete an entry with the according buttons.</p>
         <p className="help-footer">MoodPad was built by <a href="https://github.com/timjungmann">Tim Jungmann</a> using React</p>
       </div>
     </div>
